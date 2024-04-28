dfcfae8fb93d95719c97265b0a13396cb798cf52
italo Vinicius <italorodrigues476@icloud.com>
2024-04-28 15:48:26 +0100

    atualizando o text

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={[styles.container]}>
+      <View
+      onTouchStart={(event)=>{
+        Alert.alert('TOQUE', 'clique iniciado')
+      }}
+      onTouchEnd={(evento)=>{
+        Alert.alert('TOQUE', 'Toque finalizado')
+      }}
+      >
+        <Text style={[styles.texto,styles.border]}>Olá</Text>
+
+      </View>
+
+      <Text
+      selectable={true} 
+      style={[styles.texto, styles.border]}>Go run!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e15f41',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  border: {
+    borderColor: 'red',
+    borderWidth: 3
+  },
+  texto: {
+    fontSize:20,
+    fontWeight:'bold',
+    color:'#0abde3'
+  }
+  // ,
+  // texto2: {
+  //   fontSize:30,
+  //   fontWeight:'bold',
+  //   color:'#c8d6e5'
+  // }
 });
